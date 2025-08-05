e2185166b9321699e51177c3d2d5f508ca639015
SamuelFilgueira <leumas685@gmail.com>
2025-08-05 05:55:59 -0300

    Criando o repositorio de usuarios

---
+++ Users.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Users.Application.DTOs;
+
+namespace Users.Application.Interfaces;
+
+public interface IUserRepository
+{
+    Task<UserResponseDTO> GetAllUsersAsync();
+    Task<UserResponseDTO> GetUserByIdAsync(int id);
+    Task<UserResponseDTO> CreateUserAsync(CreateUserDTO userDTO);
+    Task<UserResponseDTO> UpdateUserAsync(UpdateUserDTO userDTO);
+    Task<UserResponseDTO> DeleteUserAsync(int id);
+
+}
